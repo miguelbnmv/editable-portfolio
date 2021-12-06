b580ca8d9f89dc6ecf357e58edc96c549d6c29f7
Miguel Vasconcelos <7222miguel@gmail.com>
2021-12-06 10:45:39 +0000

    Add hover effect

--- src/components/projects/project-card/ProjectCard.js
+++ src/components/projects/project-card/ProjectCard.js
@@ -1,53 +1,30 @@
-import React, { useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import Ticker from 'react-ticker';
-import { gsap } from 'gsap';
+import { useNavigate } from 'react-router-dom';
 
 import Button from 'components/shared/elements/button';
 
-import { card, cursorFollow } from './project-card.module.scss';
+import { card } from './project-card.module.scss';
 
-const ProjectCard = ({ project }) => {
+const ProjectCard = ({ project, setActiveIndex, index }) => {
   const navigate = useNavigate();
-  let tl = gsap.timeline();
-  let cursor = useRef(null);
-  let mouseX = 0;
-  let mouseY = 0;
-
-  useEffect(() => {
-    tl.to({}, 0.016, {
-      repeat: -1,
-      onRepeat: function () {
-        tl.set(cursor, {
-          css: {
-            left: mouseX - 480,
-            top: mouseY - 480,
-          },
-        });
-      },
-    });
-    document.addEventListener('mousemove', function (e) {
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      mouseX = e.pageX;
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      mouseY = e.pageY;
-    });
-  });
 
   return (
-    <>
-      <Ticker mode="chain" speed={2}>
-        {() => (
-          <div className={card} key={project.id}>
-            <Button
-              text={' ' + project.name + '.'}
-              handle={() => navigate(`/projects/${project.id}`)}
-            />
-          </div>
-        )}
-      </Ticker>
-      <div className={cursorFollow} ref={(el) => (cursor = el)}></div>
-    </>
+    <Ticker mode="chain" speed={2}>
+      {() => (
+        <div
+          className={card}
+          key={project.id}
+          onMouseEnter={() => setActiveIndex(index)}
+          onMouseLeave={() => setActiveIndex(-1)}
+        >
+          <Button
+            text={' ' + project.name + '.'}
+            handle={() => navigate(`/projects/${project.id}`)}
+          />
+        </div>
+      )}
+    </Ticker>
   );
 };
 
---
+++ src/components/projects/project-image/ProjectImage.js
@@ -0,0 +1,48 @@
+import React, { useCallback, useLayoutEffect, useState } from 'react';
+
+import { isActive } from 'pages/projects/projects-list/projects-list.module.scss';
+
+const getDimensionObject = (node) => {
+  const rect = node.getBoundingClientRect();
+
+  return {
+    width: rect.width,
+    height: rect.height,
+  };
+};
+
+const useSize = () => {
+  const [dimensions, setDimensions] = useState({});
+  const [node, setNode] = useState(null);
+
+  const ref = useCallback((node) => {
+    setNode(node);
+  }, []);
+
+  useLayoutEffect(() => {
+    if (node) {
+      const measure = () => setDimensions(getDimensionObject(node));
+      measure();
+    }
+  }, [node]);
+
+  return [ref, dimensions];
+};
+
+const ProjectImage = ({ url, active, x, y }) => {
+  const [ref, { width, height }] = useSize();
+
+  return (
+    <img
+      className={active ? isActive : null}
+      ref={ref}
+      src={url}
+      alt="project banner"
+      style={{
+        transform: `translate(${x - width / 2}px, ${y - height / 2}px)`,
+      }}
+    />
+  );
+};
+
+export default ProjectImage;
